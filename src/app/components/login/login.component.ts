2718836efef8c0fa9a9253dfd72f31adce242d46
Marco <marco.che@hotmail.it>
2022-03-18 10:46:53 +0100

    form login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { FormGroup, FormControl } from '@angular/forms';
 import { LoginService } from 'src/app/services/login/login.service';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,7 +10,7 @@ import { LoginService } from 'src/app/services/login/login.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router, private loginService: LoginService) { }
+  constructor(private router: Router, private loginService: LoginService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -23,8 +23,17 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     this.loginService.checkLogin(this.loginForm.value.user, this.loginForm.value.password).subscribe((res) => {
       localStorage.setItem('token', res.data.token);
-    });
+      this.router.navigate(['home/project']);
+    },
+      (error) => {
+        this.error();
+        this.router.navigate(['login']);
+      }
+    );
+  }
 
+  error() {
+    this.toastr.error('Dati inseriti non validi', 'Acceso non riuscito', { timeOut: 3000 });
   }
 
 }
