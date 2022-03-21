216a612d0182692512b2109e9cca27a4ee359869
Marco <marco.che@hotmail.it>
2022-03-21 10:53:47 +0100

    modali clients add edit

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import { Client } from 'src/app/models/client.model';
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
@@ -18,6 +19,8 @@ export class ClientComponent implements OnInit {
   }
 
   clientsList: any;
+
+  titleModal: any;
 
   getClients() {
     this.clientService.getClients().subscribe((res) => {
@@ -35,7 +38,7 @@ export class ClientComponent implements OnInit {
     representatives: new FormControl(''),
   });
 
-  onSubmit() {
+  addClient() {
     console.log(this.clientForm.value);
     const newClient = this.clientForm.value;
     this.clientService.addClient(newClient.name, newClient.vat_number, newClient.business_name, newClient.representatives)
@@ -52,9 +55,9 @@ export class ClientComponent implements OnInit {
       });
   }
 
-  editClient(id: any) {
+  editClient(id: any, content: any) {
     this.clientService.currentClient = id;
-    this.router.navigate(['home/client', id]);
+    this.openModalEditClient(content);
   }
 
   deleteClient(id: any, name: any, content: any) {
@@ -82,6 +85,52 @@ export class ClientComponent implements OnInit {
 
   successAddClient() {
     this.toastr.success('Operazione riuscita!', 'Aggiunto cliente', { timeOut: 3000 });
+  }
+
+  openModalAddClient(content: any) {
+    this.titleModal = "Aggiungi Cliente";
+    this.clientForm.setValue({
+      name: '',
+      vat_number: '',
+      business_name: '',
+      representatives: '',
+    });
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(() => {
+        this.addClient();
+      }, () => {
+        console.log('annullato');
+      });
+  }
+
+  client: Client;
+
+  openModalEditClient(content: any) {
+    this.titleModal = "Modifica Cliente";
+    this.clientService.getClient().subscribe((res) => {
+      this.client = res.data;
+
+      this.clientForm.setValue({
+        name: this.client.name,
+        vat_number: this.client.vat_number,
+        business_name: this.client.business_name,
+        representatives: this.client.representatives,
+      });
+    })
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(() => {
+        const client = this.clientForm.value;
+        this.clientService.updateClient(client.name, client.vat_number, client.business_name, client.representatives)
+          .subscribe(() => {
+            console.log('ok');
+            this.toastr.success('Operazione riuscita!', 'Modificato cliente', { timeOut: 3000 });
+            this.getClients();
+          })
+      }, () => {
+        console.log('annullato');
+      });
   }
 
 }
