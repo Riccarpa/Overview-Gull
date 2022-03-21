6dac3be308b3096ccf89e0ee952de576962033d6
Riccarpa <riccardopagni99@gmail.com>
2022-03-21 15:21:34 +0100

    user update in progress

--- src/app/components/user/update-user/update-user.component.ts
+++ src/app/components/user/update-user/update-user.component.ts
@@ -3,7 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { UserService } from 'src/app/services/user/user.service';
 import { User } from 'src/app/models/user.model';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ImageCropperComponent, CropperSettings } from 'ngx-img-cropper';
+
 
 @Component({
   selector: 'app-update-user',
@@ -11,13 +15,71 @@ import { FormGroup,FormControl } from '@angular/forms';
   styleUrls: ['./update-user.component.scss']
 })
 export class UpdateUserComponent implements OnInit {
-  constructor(private uService:UserService,private route:ActivatedRoute,private location:Location) { }
+  constructor(private uService:UserService,private route:ActivatedRoute,private location:Location, private fb: FormBuilder,
+    private toastr: ToastrService, private modalService: NgbModal) {
+
+      this.cropperSettings = new CropperSettings();
+      // this.cropperSettings.width = 100;
+      // this.cropperSettings.height = 100;
+      // this.cropperSettings.croppedWidth = 100;
+      // this.cropperSettings.croppedHeight = 100;
+      // this.cropperSettings.canvasWidth = 400;
+      // this.cropperSettings.canvasHeight = 300;
+      this.cropperSettings.cropperDrawSettings.lineDash = true;
+      this.cropperSettings.cropperDrawSettings.dragIconStrokeWidth = 0;
+
+      this.data = {};
+     }
   
   id = this.route.snapshot.paramMap.get('id');
   user:User
   profileForm:any
 
+  formBasic: FormGroup;
+  loading: boolean;
+  radioGroup: FormGroup;
+  confirmResut;
+  data: any;
+  cropperSettings: CropperSettings;
+
   ngOnInit(): void {
+    this.retrieveUser()
+    this.buildFormBasic();
+    this.radioGroup = this.fb.group({
+      radio: []
+    });
+  }
+
+  
+  buildFormBasic() {
+    this.formBasic = this.fb.group({
+      experience: []
+    });
+  }
+
+  submit() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+      this.toastr.success('Profile updated.', 'Success!', {progressBar: true});
+    }, 3000);
+  }
+
+  onSubmit(){
+    this.loading = true;
+    this.uService.updateUser(this.profileForm.value,this.id).subscribe(res=>{
+      this.loading = false;
+      this.toastr.success('Profile updated.', 'Success!', {progressBar: true});
+      setTimeout(() => {
+        this.back()
+      }, 3000);
+    },(error)=>{
+      this.toastr.error(error.error.message,'Error!',{progressBar: true});
+      this.loading = false
+    });
+    
+  }
+  retrieveUser(){
     this.uService.retrieveUser(this.id).subscribe((res:any)=>{
       this.user = res.data;
       this.profileForm = new FormGroup({
@@ -30,28 +92,43 @@ export class UpdateUserComponent implements OnInit {
         recruitment_date: new FormControl(this.user. recruitment_date),
         week_working_hours: new FormControl(this.user.week_working_hours),
       })
+      this.data.image = this.user.picture
     })
-  }
-  onSubmit(){
-  
-    this.uService.updateUser(this.profileForm.value,this.id).subscribe(res=>{
-      alert('User updated successfully');
-      this.location.back()
-    },(error)=>{
-      alert(error.error.message)
-    });
-    
   }
   deleteUser(){
     this.uService.deleteUser(this.id).subscribe(res=>{
-      alert('User deleted successfully');
-      this.location.back()
+      this.modalService.dismissAll()
+      this.toastr.success('Profile deleted', 'Success!', {progressBar: true});
+      setTimeout(() => {
+        this.back()
+      }, 3000);
     },(error)=>{
-      alert(error.error.message)
+      this.toastr.error(error.error.message,'Error!',{progressBar: true});
     });
   }
+  updateImg(){
+    console.log(this.data?.image)
+  }
+
+
+  confirm(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', centered: true })
+    .result.then((result) => {
+      this.confirmResut = `Closed with: ${result}`;
+    }, (reason) => {
+      this.confirmResut = `Dismissed with: ${reason}`;
+    });
+  }
+  open(modal) {
+    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title' })
+    .result.then((result) => {
+      console.log(result);
+    }, (reason) => {
+      
+    });
+  }
+  
   back(){
     this.location.back()
   }
-
 }
