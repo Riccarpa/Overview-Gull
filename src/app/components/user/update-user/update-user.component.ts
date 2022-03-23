e793c19356784b96dcb7294ca68393a85287d2a5
Riccarpa <riccardopagni99@gmail.com>
2022-03-23 12:28:03 +0100

    fix route update comp

--- src/app/components/user/update-user/update-user.component.ts
+++ src/app/components/user/update-user/update-user.component.ts
@@ -7,7 +7,7 @@ import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ImageCropperComponent, CropperSettings } from 'ngx-img-cropper';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-user',
@@ -16,18 +16,10 @@ import { ImageCropperComponent, CropperSettings } from 'ngx-img-cropper';
 })
 export class UpdateUserComponent implements OnInit {
   constructor(private uService:UserService,private route:ActivatedRoute,private location:Location, private fb: FormBuilder,
-    private toastr: ToastrService, private modalService: NgbModal) {
-
+    private toastr: ToastrService, private modalService: NgbModal,private router:Router) {
       this.cropperSettings = new CropperSettings();
-      // this.cropperSettings.width = 100;
-      // this.cropperSettings.height = 100;
-      // this.cropperSettings.croppedWidth = 100;
-      // this.cropperSettings.croppedHeight = 100;
-      // this.cropperSettings.canvasWidth = 400;
-      // this.cropperSettings.canvasHeight = 300;
       this.cropperSettings.cropperDrawSettings.lineDash = true;
       this.cropperSettings.cropperDrawSettings.dragIconStrokeWidth = 0;
-
       this.data = {};
      }
   
@@ -50,39 +42,11 @@ export class UpdateUserComponent implements OnInit {
   }
 
   
-  buildFormBasic() {
-    this.formBasic = this.fb.group({
-      experience: []
-    });
-  }
-
-  submit() {
-    this.loading = true;
-    setTimeout(() => {
-      this.loading = false;
-      this.toastr.success('Profile updated.', 'Success!', {progressBar: true});
-    }, 3000);
-  }
-
-  onSubmit(){
-    this.loading = true;
-    this.uService.updateUser(this.profileForm.value,this.id).subscribe(res=>{
-      this.loading = false;
-      this.toastr.success('Profile updated.', 'Success!', {progressBar: true});
-      setTimeout(() => {
-        this.back()
-      }, 3000);
-    },(error)=>{
-      this.toastr.error(error.error.message,'Error!',{progressBar: true});
-      this.loading = false
-    });
-    
-  }
+  
   retrieveUser(){
     this.uService.retrieveUser(this.id).subscribe((res:any)=>{
       this.user = res.data;
       if(!this.user.picture){
-
         this.user.picture = `/images/users/${this.user.id}`
       }
       this.profileForm = new FormGroup({
@@ -98,6 +62,7 @@ export class UpdateUserComponent implements OnInit {
       this.data.image = `http://80.211.57.191/${this.user.picture}`
     })
   }
+  
   deleteUser(){
     this.uService.deleteUser(this.id).subscribe(res=>{
       this.modalService.dismissAll()
@@ -109,21 +74,21 @@ export class UpdateUserComponent implements OnInit {
       this.toastr.error(error.error.message,'Error!',{progressBar: true});
     });
   }
-  confirm(content) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', centered: true })
-    .result.then((result) => {
-      this.confirmResut = `Closed with: ${result}`;
-    }, (reason) => {
-      this.confirmResut = `Dismissed with: ${reason}`;
+  
+  // update user
+  onSubmit(){
+    this.loading = true;
+    this.uService.updateUser(this.profileForm.value,this.id).subscribe(res=>{
+      this.loading = false;
+      this.toastr.success('Profile updated.', 'Success!', {progressBar: true});
+      setTimeout(() => {
+        this.router.navigate(['home/user'])
+      }, 3000);
+    },(error)=>{
+      this.toastr.error(error.error.message,'Error!',{progressBar: true});
+      this.loading = false
     });
-  }
-  open(modal) {
-    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title' })
-    .result.then((result) => {
-      console.log(result);
-    }, (reason) => {
     
-    });
   }
   updateImg(){
     
@@ -139,6 +104,28 @@ export class UpdateUserComponent implements OnInit {
     }
   }
   
+  
+  // modals
+  confirm(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', centered: true })
+    .result.then((result) => {
+      this.confirmResut = `Closed with: ${result}`;
+    }, (reason) => {
+      this.confirmResut = `Dismissed with: ${reason}`;
+    });
+  }
+  open(modal) {
+    this.modalService.open(modal, { ariaLabelledBy: 'modal-basic-title' })
+    .result.then((result) => {
+      console.log(result);
+    }, (reason) => { });
+  }
+  
+  buildFormBasic() {
+    this.formBasic = this.fb.group({
+      experience: []
+    });
+  }
   back(){
     this.location.back()
   }
