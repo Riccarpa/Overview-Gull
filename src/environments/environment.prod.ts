689550d63f1e9147b315a3236aee83683f969de8
Riccarpa <riccardopagni99@gmail.com>
2022-03-30 16:28:19 +0200

    env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   title: 'Prod Environment',
-  apiURL: 'http://80.211.57.191/api',
+  apiURL: 'http://80.211.57.191/overview_dev/api',
   apiURL2: 'http://80.211.57.191/overview_dev'
 };
