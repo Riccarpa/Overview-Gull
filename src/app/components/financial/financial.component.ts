98183093b39283a5d5128cdc3b1e9f9513779544
Riccarpa <riccardopagni99@gmail.com>
2022-03-31 18:30:28 +0200

    retrieve user

--- src/app/components/financial/financial.component.ts
+++ src/app/components/financial/financial.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FinancialService } from 'src/app/services/financial/financial.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-financial',
@@ -8,9 +10,13 @@ import { FinancialService } from 'src/app/services/financial/financial.service';
 })
 export class FinancialComponent implements OnInit {
 
-  constructor(private fService:FinancialService) { }
-
+  constructor(private fService:FinancialService,private uService:UserService,private route:ActivatedRoute,) { }
+  id = this.route.snapshot.paramMap.get('id');
+  user:any
   ngOnInit(): void {
+    this.uService.retrieveUser(this.id).subscribe((res:any)=>{
+      this.user = res.data;
+    })
   }
   
 
