27ebe3d166ee97c92b7b56a041a23d95f29954aa
Riccarpa <riccardopagni99@gmail.com>
2022-04-01 10:30:27 +0200

    get acrivities

--- src/app/components/financial/financial.component.ts
+++ src/app/components/financial/financial.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ActivitiesService } from 'src/app/services/activities/activities.service';
 import { FinancialService } from 'src/app/services/financial/financial.service';
 import { UserService } from 'src/app/services/user/user.service';
 
@@ -10,12 +11,18 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class FinancialComponent implements OnInit {
 
-  constructor(private fService:FinancialService,private uService:UserService,private route:ActivatedRoute,) { }
+  constructor(private fService:FinancialService,private uService:UserService,private route:ActivatedRoute,private aService:ActivitiesService) { }
   id = this.route.snapshot.paramMap.get('id');
   user:any
+  activities:any
   ngOnInit(): void {
     this.uService.retrieveUser(this.id).subscribe((res:any)=>{
       this.user = res.data;
+      console.log('user',this.user)
+    })
+    this.aService.getActivities().subscribe((res:any)=>{
+      this.activities = res.data;
+      console.log('activities',this.activities)
     })
   }
   
