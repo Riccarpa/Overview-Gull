a27ded791816e7c4dee1204e0ad08a9daf228fca
Marco <marco.che@hotmail.it>
2022-04-04 11:11:24 +0200

    add sprint

--- src/app/components/project/sprint/sprint.component.ts
+++ src/app/components/project/sprint/sprint.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Project } from 'src/app/models/project.model';
 import { Sprint } from 'src/app/models/sprint.model';
 import { SprintService } from 'src/app/services/sprint/sprint.service';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectService } from 'src/app/services/project/project.service';
 @Component({
   selector: 'app-sprint',
   templateUrl: './sprint.component.html',
@@ -10,11 +12,32 @@ import { SprintService } from 'src/app/services/sprint/sprint.service';
 })
 export class SprintComponent implements OnInit {
 
-  constructor(private sprintService: SprintService) { }
+  constructor(private sprintService: SprintService, private modalService: NgbModal, private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    // recupera tutti gli sprint del progetto
     this.currentSprintsIds = this.project.sprint_ids;
+    this.getSprints();
 
+  }
+
+  @Input() project: Project;
+
+  allSprints: Sprint[];
+  currentSprintsIds: number[];
+  currentSprints: Sprint[];
+  titleModal: string;
+
+  sprintForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    start_date: new FormControl('', Validators.required),
+    end_date: new FormControl('', Validators.required),
+    effort_days: new FormControl(''),
+    revenue: new FormControl(''),
+  });
+
+  // recupera tutti i sprint e li filtra per progetto
+  getSprints() {
     this.sprintService.getSprints().subscribe((res) => {
       this.allSprints = res.data;
       this.currentSprints = this.allSprints.filter((sprint) => {
@@ -25,12 +48,50 @@ export class SprintComponent implements OnInit {
         }
       });
     });
-
   }
 
-  @Input() project: Project;
+  // apre la modale per aggiungere un nuovo sprint
+  openModalAddSprint(content: any) {
+    this.titleModal = "Aggiungi Sprint";
+    this.sprintForm.setValue({
+      name: '',
+      start_date: '',
+      end_date: '',
+      effort_days: '',
+      revenue: '',
+    });
 
-  allSprints: Sprint[];
-  currentSprintsIds: number[];
-  currentSprints: Sprint[];
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(() => {
+        console.log('ok');
+      }, () => {
+        console.log('annullato');
+      });
+  }
+
+  // aggiunge un nuovo sprint al progetto
+  addSprint() {
+    if (this.sprintForm.status == 'INVALID') {
+      this.projectService.warningBar('Tutti i campi sono obbligatori');
+    } else {
+      const newSprint = this.sprintForm.value;
+      this.sprintService.addSprint(newSprint.name, newSprint.start_date, newSprint.end_date, newSprint.effort_days, newSprint.revenue, this.project.id)
+        .subscribe(() => {
+          this.modalService.dismissAll();
+          this.sprintForm.setValue({
+            name: '',
+            start_date: '',
+            end_date: '',
+            effort_days: '',
+            revenue: '',
+          });
+
+          this.projectService.getUpdateProject().subscribe((res) => {
+            this.currentSprintsIds = res.data.sprint_ids;
+            this.getSprints();
+          });
+          this.projectService.successBar('Sprint aggiunto con successo!');
+        });
+    }
+  }
 }
--- src/app/services/sprint/sprint.service.ts
+++ src/app/services/sprint/sprint.service.ts
@@ -35,4 +35,24 @@ export class SprintService {
 
     return this.http.get(url, { headers });
   }
+
+  addSprint(name: string, start_date: Date, end_date: Date, effort_days: number, revenue: number, projectId: number): Observable<any> {
+    const url = this.url;
+
+    let body = {
+      "name": name,
+      "start_date": start_date,
+      "end_date": end_date,
+      "effort_days": effort_days,
+      "revenue": revenue,
+      "project_id": projectId,
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.token}`,
+    });
+
+    return this.http.post(url, body, { headers: headers });
+  }
 }
