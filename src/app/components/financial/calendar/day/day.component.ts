fbedfa5ac70ce88fe09742e13abf03f08cee29ee
Riccarpa <riccardopagni99@gmail.com>
2022-04-06 12:45:47 +0200

    data bind

--- src/app/components/financial/calendar/day/day.component.ts
+++ src/app/components/financial/calendar/day/day.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-day',
@@ -9,7 +9,10 @@ export class DayComponent implements OnInit {
 
   constructor() { }
 
+  @Input() day : any
+  @Input() activities :any
   ngOnInit(): void {
+    
   }
 
 }
