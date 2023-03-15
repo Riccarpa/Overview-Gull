adfb0d0bec780774335579cd1ea1ce29c15dcf5c
r.pagni@h2app.it <r.pagni@h2app.it>
2023-03-15 16:12:42 +0100

    change prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   title: 'Prod Environment',
-  apiURL: 'http://80.211.57.191/overview_dev/api',
-  apiURL2: 'http://80.211.57.191/overview_dev'
+  apiURL: 'https://apps.h2app.it/overview/api',
+  apiURL2: 'https://apps.h2app.it/overview'
 };
