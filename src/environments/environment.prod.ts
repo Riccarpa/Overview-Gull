d112041fa7a0102edfd599855573e6e2101052d0
r.pagni <r.pagni@h2app.it>
2023-03-20 14:23:46 +0100

    change env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   title: 'Prod Environment',
-  apiURL: 'https://apps.h2app.it/overview/api',
-  apiURL2: 'https://apps.h2app.it/overview'
+  apiURL: 'https://apps.h2app.it/overviewdev',
+  apiURL2: 'https://apps.h2app.it/overviewdev'
 };
