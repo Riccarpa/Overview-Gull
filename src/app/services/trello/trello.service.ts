bef91eb30f4e499879fd5271b97dfc5229501e69
unknown <veronikakuchtova95@gmail.com>
2023-03-29 18:00:07 +0200

    card order + checkboxes

---
+++ src/app/services/trello/trello.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrelloService {
+
+  constructor() { }
+
+  // getUserTaskTables(id: any): Observable<any> {
+  //   return this.http.get(`${this.url}/trello/${id}`);
+  // }
+
+}
