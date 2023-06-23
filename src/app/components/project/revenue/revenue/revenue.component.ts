fdf86ac3dd38eaeb192ca89c547f683a35d1448b
r.pagni@h2app.it <r.pagni@h2app.it>
2023-06-23 14:48:30 +0200

    create revenueArr

--- src/app/components/project/revenue/revenue/revenue.component.ts
+++ src/app/components/project/revenue/revenue/revenue.component.ts
@@ -19,9 +19,25 @@ export class RevenueComponent implements OnInit {
   }
 
   createRevenueArr(){
-    console.log(this.project.users_costs)
-    console.log(this.project.user_details)
+    this.revenueArr = []
+    for (const key in this.project.users_costs["total_cost_by_user"]) {
+      if (Object.prototype.hasOwnProperty.call(this.project.users_costs["total_cost_by_user"], key)) {
+        const element = this.project.users_costs["total_cost_by_user"][key];
 
+        for (let j = 0; j < this.project.user_details.length; j++) {
+          const user = this.project.user_details[j];
+          if(key == user.id){
+            const newCostEl = {"name":user.name,"surname":user.surname,"total_hours":element.total_hours,"total":element.total}
+            this.revenueArr.push(newCostEl)
+          }
+        }
+      }
+    }
+    console.log(this.revenueArr)
   }
+
+   
+
+
 
 }
